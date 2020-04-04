39be87efdc1dfd48b099182ff608fec2b19956cf
System Administrator <root@lals-MacBook-Air.local>
2020-04-04 14:16:55 +0530

    added port

--- index.js
+++ index.js
@@ -49,6 +49,6 @@ app.get('/test',(req,res)=>{
 })
 
 // Finally, start our server
-app.listen(3000, function() {
+app.listen(process.env.PORT || 3000, function() {
   console.log('Telegram app listening on port 3000!')
 })
